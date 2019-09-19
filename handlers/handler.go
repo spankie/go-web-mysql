7fc9bd2e7320dfc1b15d6a6e2c0186c5dc09c78e
Odohi David <odohi.david@gmail.com>
2019-09-19 11:07:47 +0100

    add signup handler to app

--- handlers/handler.go
+++ handlers/handler.go
@@ -1,11 +1,14 @@
 package handlers
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
 	"path"
+
+	"github.com/spankie/go-web-mysql/db"
 )
 
 // HomeHandler serves index page
@@ -36,7 +39,32 @@ func SignupHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Println(err)
 	}
-	err = t.Execute(w, nil) // merge.
+	user := User{}
+	r.ParseForm()
+	user.Email = r.Form.Get("email")
+	user.Password = r.Form.Get("password")
+	log.Println(user)
+	if len(user.Email) < 1 || len(user.Password) < 1 {
+		log.Println(err)
+		message := "Username or password incorrect"
+		err = t.Execute(w, message) // merge.
+		if err != nil {
+			log.Println(err)
+		}
+		// status := http.StatusUnauthorized
+		return
+	}
+
+	// perform a db.Query insert
+	insert, err := db.DB.Query(fmt.Sprintf("INSERT INTO users VALUES ( NULL, '%s', '%s' )", user.Email, user.Password))
+
+	// if there is an error inserting, handle it
+	if err != nil {
+		panic(err.Error())
+	}
+	// be careful deferring Queries if you are using transactions
+	defer insert.Close()
+	err = t.Execute(w, user) // merge.
 	if err != nil {
 		log.Println(err)
 	}
