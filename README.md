a0277e5d669f26d858ebfb356e2243f01c239f43
Odohi David <odohi.david@gmail.com>
2019-09-19 09:58:55 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Golang website boilerplate
+
+Dependencies:
+
+- golang routing: [go-chi](https://github.com/go-chi/chi)
+
+- Stylesheet (CSS): [tachyons.io](https://tachyons.io)
---
+++ handlers/handler.go
@@ -0,0 +1,43 @@
+package handlers
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"path"
+)
+
+// HomeHandler serves index page
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	t := template.New("index.html") // Create a template.
+	cwd, _ := os.Getwd()
+	p := path.Join(cwd, "public", "index.html")
+	t, err := t.ParseFiles(p) // Parse template file.
+	if err != nil {
+		log.Println(err)
+	}
+	err = t.Execute(w, nil) // merge.
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+type User struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func SignupHandler(w http.ResponseWriter, r *http.Request) {
+	t := template.New("index.html") // Create a template.
+	cwd, _ := os.Getwd()
+	p := path.Join(cwd, "public", "index.html")
+	t, err := t.ParseFiles(p) // Parse template file.
+	if err != nil {
+		log.Println(err)
+	}
+	err = t.Execute(w, nil) // merge.
+	if err != nil {
+		log.Println(err)
+	}
+}
