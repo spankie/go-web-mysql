672abf6a703b7bf098b35979ed645ffdb8c8336c
Odohi David <odohi.david@gmail.com>
2019-09-19 20:39:31 +0100

    chamge the port to :8080 and add some comments

--- main.go
+++ main.go
@@ -12,14 +12,14 @@ import (
 )
 
 func main() {
-	r := chi.NewRouter()
+	r := chi.NewRouter() // creating a new router
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
 	// get the pointer to the DB so that it can be closed
 	// after the main function returns
 	DB := db.GetDB()
-	defer DB.Close()
+	defer DB.Close() // Idiomatic go
 
 	fileServer := http.StripPrefix("/assets/", http.FileServer(http.Dir("./public/assets")))
 	r.Get("/assets/*", func(w http.ResponseWriter, r *http.Request) {
@@ -27,13 +27,15 @@ func main() {
 		w.Header().Set("Cache-Control", "public, max-age=7776000")
 		fileServer.ServeHTTP(w, r)
 	})
-
+	//PARSE
 	r.Get("/", handlers.HomeHandler)
 	r.Post("/signup", handlers.SignupHandler)
+	r.Get("/login", handlers.SignupHandler)
+	r.Post("/login", handlers.SignupHandler)
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./public/404.html")
 	})
-	log.Print("👉  Server started at 127.0.0.1:3333")
-	http.ListenAndServe(":3333", r)
+	log.Print("👉  Server started at 127.0.0.1:8080")
+	http.ListenAndServe(":8080", r)
 }
